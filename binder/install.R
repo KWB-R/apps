52e4bf6e72bcce90fce3cb9ae9700727dadb2c4c
mrustl <michael.rustler@kompetenz-wasser.de>
2018-07-12 01:15:46 +0200

    add app for haridwar (aquanes.report@v.0.5.0)
    (copied app from aquanes.report package, see commit: https://github.com/KWB-R/aquanes.report/commit/3cdf515835793b8e3bbab1b003a1865a3dff34ef)

---
+++ binder/install.R
@@ -0,0 +1,2 @@
+install.packages("caTools", "bitops", "devtools")
+devtools::install_github("kwb-r/aquanes.report@v.0.5.0", dependencies = TRUE)
---
+++ haridwar/global.R
@@ -0,0 +1,60 @@
+use_live_data <- FALSE
+
+if (use_live_data) {
+
+library(aquanes.report)
+
+analytics <- file.path(getwd(),
+                       "data/analytics.xlsx")
+
+mySQL <- file.path(getwd(),
+                   ".my.cnf")
+
+op_meta <- file.path(getwd(),
+                     "data/operation_parameters.csv")
+
+system.time(
+siteData_raw_list <- aquanes.report::import_data_haridwar(analytics_path = analytics,
+                                          operation_mySQL_conf = mySQL,
+                                          operation_meta_path = op_meta))
+
+print("### Step 4: Performing temporal aggregation ##########################")
+system.time(
+siteData_10min_list <- aquanes.report::group_datetime(siteData_raw_list,
+                                                      by = 10*60))
+
+system.time(
+siteData_hour_list <- aquanes.report::group_datetime(siteData_raw_list,
+                                                     by = 60*60))
+
+system.time(
+  siteData_day_list <- aquanes.report::group_datetime(siteData_raw_list,
+                                                        by = "day"))
+
+
+
+saveRDS(siteData_raw_list, file = "data/siteData_raw_list.Rds")
+saveRDS(siteData_10min_list, file = "data/siteData_10min_list.Rds")
+saveRDS(siteData_hour_list, file = "data/siteData_hour_list.Rds")
+saveRDS(siteData_day_list, file = "data/siteData_day_list.Rds")
+
+} else {
+  #siteData_raw_list <- readRDS("data/siteData_raw_list.Rds")
+  siteData_10min_list <- readRDS("data/siteData_10min_list.Rds")
+  #siteData_hour_list <- readRDS("data/siteData_hour_list.Rds")
+  #siteData_day_list <- readRDS("data/siteData_day_list.Rds")
+}
+
+print("### Step 5: Importing threshold information ##########################")
+thresholds <- aquanes.report::get_thresholds()
+
+# Limit reporting time range to available data
+data_timerange <- as.Date(paste0(format(range(siteData_10min_list$DateTime), format = "%Y-%m"), "-01"))
+
+print("### Step 6: Specify available months for reporting ##########################")
+report_months <- aquanes.report::create_monthly_selection(startDate = as.character(data_timerange[1]),
+                                                          endDate = data_timerange[2])
+
+
+print("### Step 7: Add default calculated operational parameters ##########################")
+report_calc_paras <- unique(aquanes.report::calculate_operational_parameters(df = siteData_10min_list)$ParameterName)
