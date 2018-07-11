9448412283f9e18789655209caa9e2337a26836c
mrustl <michael.rustler@kompetenz-wasser.de>
2018-07-12 00:31:51 +0200

    fix typo

--- binder/install.R
+++ binder/install.R
@@ -1,4 +1,4 @@
 pkgs_cran <- c("shiny", "shinythemes", "digest", 
 "metricsgraphics", "leaflet", "kwb.hantush", 
 "rmarkdown", "caTools", "bitops")
-install.package(pkgs_cran)
+install.packages(pkgs_cran)
