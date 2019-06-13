2a03780aafe0e3a891ce39ff87dbe5e253936512
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2019-06-13 06:30:24 +0200

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,2 +1,2 @@
-install.packages("devtools")
-devtools::install_github("kwb-r/aquanes.report@v.0.5.0", dependencies = TRUE)
+install.packages("remotes")
+remotes::install_github("kwb-r/aquanes.report@v.0.5.0", dependencies = TRUE)
