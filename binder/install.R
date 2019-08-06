82f25380771b021f6c3a6f326b52e47828863d76
mrustl <michael.rustler@kompetenz-wasser.de>
2019-08-06 13:01:17 +0200

    Set version of fakin.path.app (to v0.1.0)

--- binder/install.R
+++ binder/install.R
@@ -1,2 +1,2 @@
 install.packages("remotes")
-remotes::install_github("kwb-r/fakin.path.app@dev", dependencies = TRUE)
+remotes::install_github("kwb-r/fakin.path.app@8f0597cd86ba340a269de7e07912595f4a8f3833", dependencies = TRUE)
