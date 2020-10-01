b8f1e69a71f7a6ec2502daabfa583492f1ff399b
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2020-10-01 11:14:05 +0200

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,2 +1,2 @@
 install.packages("remotes")
-remotes::install_github("kwb-r/fakin.path.app@v0.3.0", auth_token = "a3b1873c1f175b0e40bd24bbd0db4b672e1fcbad", dependencies = TRUE)
+remotes::install_github("kwb-r/fakin.path.app@v0.3.0", upgrade = "never")
